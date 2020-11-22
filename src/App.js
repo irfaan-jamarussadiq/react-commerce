1c74e297c9d1566b7bd42f6cdbdd8c591ec5eb72
irfaan-jamarussadiq <irfaanj9@gmail.com>
2020-11-22 12:43:15 -0500

    centered nav elements properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import NavBar from "./NavBar.js";
-import LandingPage from "./LandingPage.js";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
-      <LandingPage />
     </div>
   );
 }
