3f00f16d1ddc9488a3a840fa6c9a453a6a139c7a
irfaan-jamarussadiq <irfaanj9@gmail.com>
2020-11-22 12:39:25 -0500

    edit

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,19 @@
 import React from "react";
-import logo from "../public/images/react-logo.PNG";
+import logo from "./images/react-logo.png";
 
 class NavBar extends React.Component {
   render() {
     return (
       <div className="NavBar">
         <ul id="nav-list">
-          <img src={logo} alt="react-logo" className="logo" />
-  
           <li>
-            <a href="#">Home</a>
+            <a href="#">Shop</a>
           </li>
           <li>
-            <a href="#">Gallery</a>
+            <a href="#">Collections</a>
           </li>
           <li>
-            <a href="#">Contact Us</a>
+            <a href="#">Account</a>
           </li>
         </ul>
       </div>
