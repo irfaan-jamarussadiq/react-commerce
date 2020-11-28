c8f7655801df0666f0b60bf409b67bcba0d3c3e8
irfaan-jamarussadiq <irfaanj9@gmail.com>
2020-11-28 08:55:22 -0500

    added more items and spaced them properly

--- src/ProductList.js
+++ src/ProductList.js
@@ -20,26 +20,26 @@ export default function ProductList() {
                     </div>
                     <div className="gallery-item">
                         <figure class="gallery__item gallery__item--2">
-                            <img src="images/bedroom.jpg" class="gallery__img" alt="Image 2" />
+                            <img src="images/small-table.jpg" class="gallery__img" alt="Image 2" />
                         </figure>
                         <div className="product-name-container">
-                            <h3 className="product-name">Queen Size Bed</h3>
+                            <h3 className="product-name">Small Study Table</h3>
                         </div>
 
                         <div className="product-price-container">
-                            <h3 className="product-price">$350.99</h3>
+                            <h3 className="product-price">$19.99</h3>
                         </div>
                     </div>
                     <div className="gallery-item">
                         <figure class="gallery__item gallery__item--3">
-                            <img src="images/bedroom.jpg" class="gallery__img" alt="Image 3" />
+                            <img src="images/sofa.jpg" class="gallery__img" alt="Image 3" />
                         </figure>
                         <div className="product-name-container">
-                            <h3 className="product-name">Queen Size Bed</h3>
+                            <h3 className="product-name">Medium Size Sofa</h3>
                         </div>
 
                         <div className="product-price-container">
-                            <h3 className="product-price">$350.99</h3>
+                            <h3 className="product-price">$250.99</h3>
                         </div>
                     </div>
                     <div className="gallery-item">
@@ -76,6 +76,32 @@ export default function ProductList() {
 
                         <div className="product-price-container">
                             <h3 className="product-price">$109.99</h3>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item">
+                        <figure class="gallery__item gallery__item--3">
+                            <img src="images/work-desk.jpg" class="gallery__img" alt="Image 3" />
+                        </figure>
+                        <div className="product-name-container">
+                            <h3 className="product-name">Work Desk</h3>
+                        </div>
+
+                        <div className="product-price-container">
+                            <h3 className="product-price">$99.99</h3>
+                        </div>
+                    </div>
+
+                    <div className="gallery-item">
+                        <figure class="gallery__item gallery__item--3">
+                            <img src="images/sofa2.jpg" class="gallery__img" alt="Image 3" />
+                        </figure>
+                        <div className="product-name-container">
+                            <h3 className="product-name">5 Seat Sofa</h3>
+                        </div>
+
+                        <div className="product-price-container">
+                            <h3 className="product-price">$450.99</h3>
                         </div>
                     </div>
                 </div>
