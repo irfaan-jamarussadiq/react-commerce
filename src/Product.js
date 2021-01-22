9cb6c66f2ad2aa683720ea4f29406c166bccd6d4
Irfaan <irfaanj9@gmail.com>
2021-01-21 23:26:51 -0500

    created todo for product class, added image and name for product (just the basics)

--- src/Product.js
+++ src/Product.js
@@ -2,10 +2,19 @@ export default function Product({ title, img_path, img_class, img_alt, figure_cl
     return (
         <div className={"product"}>
             <div className="product-content" id={product_id}>
-                <h1>{title}</h1>
+                <h2>{title}</h2>
                 <figure class={figure_class}>
                     <img src={img_path} class={img_class} alt={img_alt} />
                 </figure>
+
+                {/* TODO: change this to general function parameters. */}
+                <div className="product-name-container">
+                    <h3 className="product-name">Queen Size Bed</h3>
+                </div>
+
+                <div className="product-price-container">
+                    <h3 className="product-price">$350.99</h3>
+                </div>
             </div>
         </div>
     );
